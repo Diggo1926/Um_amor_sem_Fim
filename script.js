6e08424aa8df59997a98447aef81127297910195
Rodrigo Carvalho <rodrigomamedecarvalho@gmail.com>
2025-03-19 00:11:29 -0300

    Melhoria no layout e botão de música atualizado

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-// Função para abrir o modal
-function openModal(photoId) {
-    var modal = document.getElementById("modal");
-    var modalImage = document.getElementById("modal-image");
-    var modalText = document.getElementById("modal-text");
-    var photo = document.getElementById(photoId);
-    var image = photo.getElementsByTagName("img")[0];
-    var text = photo.getElementsByClassName("photo-text")[0].innerText;
-
-    modalImage.src = image.src;
-    modalText.innerText = text;
-    modal.style.display = "block";
-}
-
-// Função para fechar o modal
-function closeModal() {
-    var modal = document.getElementById("modal");
-    modal.style.display = "none";
-}
-
-// Função para atualizar o contador de tempo
+// Contador de tempo
 function updateCounter() {
-    // Data de início
     const startDate = new Date("2024-05-30T00:00:00");
-
     const now = new Date();
     const diff = now - startDate;
 
@@ -31,12 +9,39 @@ function updateCounter() {
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-    document.getElementById('days').innerHTML = `Você está junto há ${days} dias`;
-    document.getElementById('time').innerHTML = `${hours} horas, ${minutes} minutos, e ${seconds} segundos`;
+    document.getElementById("days").innerText = `${days} dias`;
+    document.getElementById("time").innerText = `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+
+    setTimeout(updateCounter, 1000);
 }
 
-// Atualiza o contador a cada segundo
-setInterval(updateCounter, 1000);
+// Modal de imagens
+function openModal(photoId) {
+    const modal = document.getElementById("modal");
+    const modalImage = document.getElementById("modal-image");
+    const modalText = document.getElementById("modal-text");
 
-// Inicia o contador ao carregar a página
+    const photo = document.getElementById(photoId);
+    modalImage.src = photo.querySelector("img").src;
+    modalText.innerText = photo.querySelector(".photo-text").innerText;
+
+    modal.style.display = "flex";
+}
+
+function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+// Controle de áudio
+function toggleAudio() {
+    const audioPlayer = document.getElementById("audio-player");
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+    } else {
+        audioPlayer.pause();
+    }
+}
+
+// Inicializar contador
 updateCounter();
